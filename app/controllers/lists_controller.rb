58a0739397e848e199c4de385e3c88d6d362e798
mariorzgz <mario.rod.gonz97@gmail.com>
2022-02-17 15:56:25 +0100

    Finished with the show and index for movies

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,6 +12,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   private
@@ -21,6 +26,6 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :image_url)
   end
 end
