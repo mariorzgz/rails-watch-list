cbc55706a68dace628974a5cbbd3efc43252f3d1
mariorzgz <mario.rod.gonz97@gmail.com>
2022-02-18 11:55:35 +0100

    Changed default color to purple

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -27,6 +27,6 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:name, :image_url)
+    params.require(:list).permit(:name, :photo)
   end
 end
