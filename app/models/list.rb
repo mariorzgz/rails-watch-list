fb30d5c74865f54082059dd38e48a1ff443d2093
mariorzgz <mario.rod.gonz97@gmail.com>
2022-02-18 17:02:09 +0100

    Changed placeholder

--- app/models/list.rb
+++ app/models/list.rb
@@ -7,6 +7,6 @@ class List < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   def default_photo
-    photo.key.nil? ? "photo-1574894078563-01e879b89809_ufnpax" : photo.key
+    photo.key.nil? ? "placeholder" : photo.key
   end
 end
